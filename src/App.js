65c92bbe7c6786755041bb62b158a3ac9b286663
Anh Le <ale2@stevens.edu>
2022-11-15 18:23:20 -0500

    setup category and homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
+const categories = [
+  {
+    id: 1,
+    name: "smartphones",
+  },
+  {
+    id: 2,
+    name: "laptops",
+  },
+  {
+    id: 3,
+    name: "home-decoration",
+  },
+  {
+    id: 4,
+    name: "furniture",
+  },
+  {
+    id: 5,
+    name: "mens-watches",
+  },
+  {
+    id: 6,
+    name: "sunglasses",
+  },
+];
 function App() {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const myData = await fetch("https://dummyjson.com/products?limit=100");
+    let result = await myData.json();
+    result = result.products.filter(
+      (item) =>
+        item.category === "smartphones" ||
+        item.category === "laptops" ||
+        item.category === "home-decoration" ||
+        item.category === "furniture" ||
+        item.category === "mens-watches" ||
+        item.category === "sunglasses"
+    );
+    setData(result);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(categories);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <div key={category.id} className="category-container">
+            <img
+              className="category-background-image"
+              src={`${category.name}.png`}
+            />
+            <div className="category-body-container">
+              <h2>{category.name}</h2>
+              <button>Shop Now</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
