c96c7c7ee71134f2f9ea629fc06e6f7dc9307655
Anh Le <ale2@stevens.edu>
2023-01-09 20:25:41 -0500

    auto carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./carousel.styles.scss";
 import { AiOutlineArrowRight } from "react-icons/ai";
 function Carousel({ img }) {
-  const [slide, setSlide] = useState(0);
+  const [index, setIndex] = useState(0);
+  const slides = [
+    <img src="./home0.jpg" alt="" />,
+    <img src="./home1.jpg" alt="" />,
+    <img src="./home2.jpg" alt="" />,
+    <img src="./home3.jpg" alt="" />,
+  ];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % slides.length);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, [index]);
   return (
     <div className="carousel-container">
       <div className="img-container">
-        <img src="./home1.jpg" alt="" />
+        {/* <img src="./home1.jpg" alt="" /> */}
+        {slides[index]}
       </div>
       <p className="shopnow-container">Shop Now</p>
 
